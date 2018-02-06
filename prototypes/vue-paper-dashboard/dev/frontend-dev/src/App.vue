1b3918f5d43f2079035138a46bd2b03ebac54c21
Moey Design <5884seiya@gmail.com>
2018-02-06 18:14:27 -0500

    removed settings for interactive kiosks

--- prototypes/vue-paper-dashboard/dev/frontend-dev/src/App.vue
+++ prototypes/vue-paper-dashboard/dev/frontend-dev/src/App.vue
@@ -2,12 +2,7 @@
  Vue Template
 ================================================== -->
 <template>
-  <div
-    id="app"
-    :style="getSize"
-    @click="[displayRipple($event), resetAppTimer()]"
-    @mousemove="resetAppTimer()"
-  >
+  <div id="app">
     <router-view/>
   </div>
 </template>
@@ -16,83 +11,8 @@
  Vue Script
 ================================================== -->
 <script>
-import APP_CONFIG from '../static/app-config'
 export default {
-  name: 'app',
-  data () {
-    return {
-      appTimer: null
-    }
-  },
-  created () {
-    // reset the app timer
-    this.resetAppTimer()
-  },
-  mounted () {
-    this.resize()
-
-    // set an event listener for resizing
-    window.addEventListener('resize', this.resize)
-  },
-  beforeDestroy () {
-    // remove the event listener
-    window.removeEventListener('resize', this.resize)
-  },
-  computed: {
-    getSize () {
-      return {
-        width: APP_CONFIG.monitor.width + 'px',
-        height: APP_CONFIG.monitor.height + 'px'
-      }
-    }
-  },
-  methods: {
-    resize () {
-      // calc how much the content should be zoomed
-      let zoom
-
-      let ratioW = window.innerWidth / APP_CONFIG.monitor.width
-      let ratioH = window.innerHeight / APP_CONFIG.monitor.height
-      ratioW < ratioH ? zoom = ratioW : zoom = ratioH
-
-      // zoom it
-      const el = document.getElementById('app')
-      el.style.zoom = zoom
-    },
-    // display ripple effect at mouse-clicked position
-    displayRipple (event) {
-      // remove old repple element
-      const oldRippleElement = document.getElementById('touch-ripple')
-      if (oldRippleElement) {
-        oldRippleElement.parentNode.removeChild(oldRippleElement)
-      }
-
-      // calc ripple position
-      const rippleRadius = 15
-      let posX = event.clientX - rippleRadius
-      let posY = event.clientY - rippleRadius
-
-      // create new ripple element
-      const newRippleElement = document.createElement('span')
-      newRippleElement.id           = 'touch-ripple'
-      newRippleElement.style.width  = (rippleRadius * 2) + 'px'
-      newRippleElement.style.height = (rippleRadius * 2) + 'px'
-      newRippleElement.style.left   = posX + 'px'
-      newRippleElement.style.top    = posY + 'px'
-
-      const appElement = document.getElementById('app')
-      appElement.appendChild(newRippleElement)
-
-      newRippleElement.classList.add('touch-ripple-effect')
-    },
-    resetAppTimer () {
-      clearTimeout(this.appTimer)
-      this.appTimer = setTimeout(() => {
-        console.log('Timeout. Returning to Home screen.')
-        this.jumpTo('home', {dir: 'home'})
-      }, APP_CONFIG.timeout)
-    }
-  }
+  name: 'app'
 }
 </script>
 
@@ -101,7 +21,6 @@ export default {
 ================================================== -->
 <style lang='stylus'>
 @import 'reset-css/reset.css'
-@import '/stylus/base/*'
 @import '/stylus/fonts'
 @import '/stylus/global-styles'
 </style>
--- prototypes/vue-paper-dashboard/dev/frontend-dev/static/app-config.js
+++ prototypes/vue-paper-dashboard/dev/frontend-dev/static/app-config.js
@@ -1,7 +1,2 @@
 export default {
-  monitor: {
-    width: 1920,
-    height: 1080
-  },
-  timeout: 1000 * 60 * 10
 }
